d35d8098961a4ae547f0185ab335623466af596c
David Schnepp <david.schnepp@schneppd.com>
2017-06-02 15:01:59 +0200

    Basic test logic added

--- AdvancedFileAccess/app/src/main/java/org/schneppd/lab/advancedfileaccess/MainActivity.java
+++ AdvancedFileAccess/app/src/main/java/org/schneppd/lab/advancedfileaccess/MainActivity.java
@@ -2,6 +2,11 @@ package org.schneppd.lab.advancedfileaccess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // logic to test
+    public void TestFeature(View view) {
+        /*
+        LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+        try {
+            while (it.hasNext()) {
+                String line = it.nextLine();
+                /// do something with line
+            }
+        } finally {
+            LineIterator.closeQuietly(iterator);
+        }
+        */
+        TextView t =(TextView)findViewById(R.id.txtTest);
+        t.setText("Feature tested");
+    }
+
+
 }
